5166ccb877ed904b0a803e851c6552984277d24e
Steven Hutchison <hutchison.music@gmail.com>
2023-05-12 22:54:44 -0500

    fix localstorage logic again

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,25 @@ import Form from './components/Form';
 import Items from "./components/Items";
 import setStorage from "./functions/localStorage";
 
+let initialList;
+let initialDeleted;
+let nextId;
+
+const data = JSON.parse(window.localStorage.getItem("choresData"));
+if (data) {
+  initialList = [...data.list]
+  initialDeleted = [...data.deleted]
+  nextId = data.list.length > 0 ? Math.max(...data.list.map(each => Number(each.id))) + 1 : 0;
+} else {
+  initialList = [
+    {id: 2000, priority: false, complete: false, content: "Use form to add"},
+    {id: 2001, priority: false, complete: false, content: "Double click to delete"},
+    {id: 2002, priority: false, complete: false, content: "Use form to restore or clear history"}
+  ];
+  initialDeleted = [" "];
+  nextId = 0;
+}
+
 export default function App() {
   const [list, setList] = useState(initialList);
   const [deleted, setDeleted] = useState(initialDeleted);
@@ -30,14 +49,6 @@ export default function App() {
   );
 }
 
-const data = JSON.parse(window.localStorage.getItem("choresData"));
 
-const initialList = [...data.list]
-  || [
-    {id: 2000, priority: false, complete: false, content: "Use form to add"},
-    {id: 2001, priority: false, complete: false, content: "Double click to delete"},
-    {id: 2002, priority: false, complete: false, content: "Use form to restore or clear history"}
-  ];
 
-const initialDeleted = [...data.deleted] || [" "];
-let nextId = data.list.length > 0 ? Math.max(...data.list.map(each => Number(each.id))) + 1 : 0;
+
